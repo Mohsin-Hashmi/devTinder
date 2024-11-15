fad5e721c13bc6cd03d359fc1f0a1bc77cb370ed
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2024-11-15 13:03:19 +0500

    exploring CURD operations

--- src/app.js
+++ src/app.js
@@ -1,17 +1,43 @@
-const express= require('express');
-const app= express();
+const express = require("express");
+const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("Hello from the home");
-})
+// This is will only handle GET to /user
+app.get("/user", (req, res) => {
+  res.send({
+    firstName: "Mohsin",
+    lastName: "Hashmi",
+  });
+});
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello hello hello");
-})
+app.post("/user", (req, res) => {
+    // saving data to the DB
+  console.log("save data to the database");
+  res.send("data successfully saved to the database!");
+});
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from the server");
+app.put("/user", (req, res)=>{
+    res.send({
+        firstName: "Mohsin",
+        lastName: "Hashmi",
+        city: "Islamabad"
+      });
+});
+
+app.patch("/user", (req,res)=>{
+    res.send("patch response is send")
 })
-app.listen(3000, ()=>{
-    console.log('Server is successfully listening on port 3000...');
+app.delete("/user", (req,res)=>{
+    res.send(
+        {
+            lastName: "Hashmi"
+        }
+    );
+})
+// this is match all the http methods APIs calls to /test
+app.use("/test", (req, res) => {
+  res.send("test");
+});
+
+app.listen(3000, () => {
+  console.log("Server is successfully listening on port 3000...");
 });
