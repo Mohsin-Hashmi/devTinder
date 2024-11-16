77048b86372bd64274ecb838902a0f26651cbaf5
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2024-11-16 14:47:05 +0500

    writing auth middleware

--- src/app.js
+++ src/app.js
@@ -1,68 +1,21 @@
 const express = require("express");
 const app = express();
+const {adminAuth,userAuth}= require("./middlewares/auth")
 
-// // This is will only handle GET to /user
-// app.get("/user/:userId/:name/:password", (req, res) => {
-//     console.log(req.params);
-//   res.send({
-//     firstName: "Mohsin",
-//     lastName: "Hashmi",
-//   });
-// });
+ /**below this the middleware because it execute in all the request handlers */
+app.use("/admin", adminAuth);
 
-// app.post("/user", (req, res) => {
-//     // saving data to the DB
-//   console.log("save data to the database");
-//   res.send("data successfully saved to the database!");
-// });
 
-// app.put("/user", (req, res)=>{
-//     res.send({
-//         firstName: "Mohsin",
-//         lastName: "Hashmi",
-//         city: "Islamabad"
-//       });
-// });
 
-// app.patch("/user", (req,res)=>{
-//     res.send("patch response is send")
-// })
-// app.delete("/user", (req,res)=>{
-//     res.send(
-//         {
-//             lastName: "Hashmi"
-//         }
-//     );
-// })
-// // this is match all the http methods APIs calls to /test
-// app.use("/test", (req, res) => {
-//   res.send("test");
-// });
-
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("Handling the user request");
-    next();
-  },
-  (req, res, next) => {
-    console.log("First Response");
-    // res.send("First Response")
-    next();
-  },
-  (req, res, next) => {
-    console.log("Second Response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Third Response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Fourth Response")
-    res.send("Fourth Response!");
-  }
-);
+app.get("/user", userAuth, (req,res)=>{
+  res.send("user data sent");
+});
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All data sent");
+});
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("User Deleted");
+});
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000...");
