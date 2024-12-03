dd32286b15cddac0b0b1c851a361004997e04bf8
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2024-12-03 21:18:58 +0500

    developed forgot password API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,8 @@ const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const { validateEditProfileData } = require("../utils/validation");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 /**Creating the API to get the profile of the user by using userAuth middleware */
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
@@ -25,18 +27,37 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
 
     Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
     await loggedInUser.save();
-    res.json({message: `${loggedInUser.firstName} your profile updated successfully.`, data:loggedInUser});
+    res.json({
+      message: `${loggedInUser.firstName} your profile updated successfully.`,
+      data: loggedInUser,
+    });
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
 });
 
-
 /**Creating the API to Update the password of the user forget password API */
-profileRouter.patch("/profile/password", async(req,res)=>{
-  try{
-
-  }catch(err){
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    const { password, newPassword } = req.body;
+    if (!newPassword || newPassword.length < 6) {
+      throw new Error("Password must be at least six characters");
+    }
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      throw new Error("User Not Found");
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      throw new Error("Incorrect Old Password");
+    }
+    
+    /**Hash the new Password before saving it */
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
+    await user.save();
+    res.json({ message: `Password Updated Successfully `, data: user });
+  } catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
 });
