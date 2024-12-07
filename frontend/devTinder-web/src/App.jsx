cd3ec2b2290ec59d4856560c5d29b14e485eae54
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2024-12-07 10:59:23 +0500

    Developed the Footer Component

--- frontend/devTinder-web/src/App.jsx
+++ frontend/devTinder-web/src/App.jsx
@@ -1,15 +1,18 @@
-import NavBar from "./components/NavBar";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import Body from "./components/Body";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <BrowserRouter basename="/">
         <Routes>
-          <Route path="/" element={<NavBar />}></Route>
-          <Route path="/login" element= {<Login/>}></Route>
-          <Route path="/signup" element= {<Signup/>}></Route>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+            
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
